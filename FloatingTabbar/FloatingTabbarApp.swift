72e3825442c9b4991a9e92f22e6984ed27e9a2c3
Sezer Tunca <sezertunca@gmail.com>
2022-02-11 16:02:01 +0000

    Initial Commit

---
+++ FloatingTabbar/FloatingTabbarApp.swift
@@ -0,0 +1,17 @@
+//
+//  FloatingTabbarApp.swift
+//  FloatingTabbar
+//
+//  Created by Sezer Tunca on 11/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct FloatingTabbarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
